9dec3cdb993e9f40f6ab432aebfde197e007d31d
Captainsmile <1433601130@qq.com>
2025-05-03 23:07:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function login() {
   const password = document.getElementById('password').value;
-  const correctPassword = '8888'; // 设置你的密码
+  const correctPassword = '112233'; // 设置你的密码
   const redirectURL = 'https://www.kdocs.cn/l/clKYOffD8W7h'; // 设置你的金山文档链接
 
   if (password === correctPassword) {
