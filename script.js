3f8db6d6294004c1a289addc8eff889cb73362fc
Captainsmile <1433601130@qq.com>
2025-05-03 23:00:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function login() {
+  const password = document.getElementById('password').value;
+  const correctPassword = '8888'; // 设置你的密码
+  const redirectURL = 'https://www.kdocs.cn/l/clKYOffD8W7h'; // 设置你的金山文档链接
+
+  if (password === correctPassword) {
+    window.location.href = redirectURL;
+  } else {
+    document.getElementById('message').textContent = '密码错误，请重试。';
+  }
+}
